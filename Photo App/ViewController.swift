f1a97157bf765e35ba7950a965e2e499d6f82521
Homaine Blair <homaineblair@gmail.com>
2022-09-14 13:12:15 -0400

    Initial Commit

---
+++ Photo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo App
+//
+//  Created by 1a on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
